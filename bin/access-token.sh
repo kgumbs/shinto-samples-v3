9f279da46313e0ace4d1a41aa79a901d6e63a7be
Mark Washburn <mark@mrwconsulting.tech>
2023-10-31 15:30:54 -0500

    updated script

--- bin/access-token.sh
+++ bin/access-token.sh
@@ -8,19 +8,24 @@ fi
 
 DEFAULT_USERNAME='admin'
 DEFAULT_PASSWORD='Password01!'
+DEFAULT_REGION='us-east-1'
 PARAMETER_STORE_COGNITO_KEY='/mrwconsulting/shinto/cognito'
 
 read -e -p  "Enter cognito username: [$DEFAULT_USERNAME] " COGNITO_USERNAME
 COGNITO_USERNAME="${COGNITO_USERNAME:-$DEFAULT_USERNAME}"
 read -e -p  "Enter cognito password: [$DEFAULT_PASSWORD] " COGNITO_PASSWORD
 COGNITO_PASSWORD="${COGNITO_PASSWORD:-$DEFAULT_PASSWORD}"
+read -e -p  "Enter region: [$DEFAULT_REGION] " AWS_REGION
+AWS_REGION="${AWS_REGION:-$DEFAULT_REGION}"
 
 COGNITO_CONFIG=$(aws ssm get-parameter \
+                --region ${AWS_REGION} \
                 --name ${PARAMETER_STORE_COGNITO_KEY} \
                 --query Parameter.Value \
                 --output text | base64 --decode)
 COGNITO_CLIENT_ID=$(echo $config | jq --raw-output .userPoolClientId)
 ACCESS_TOKEN=$(aws cognito-idp initiate-auth  \
+            --region ${AWS_REGION} \
             --client-id ${COGNITO_CLIENT_ID} \
             --auth-flow USER_PASSWORD_AUTH \
             --query "AuthenticationResult.IdToken" \
