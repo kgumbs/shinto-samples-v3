75c844484d1563776fcfffa8dd5a82c700ce8ac9
Mark Washburn <mark@mrwconsulting.tech>
2024-02-02 16:59:50 -0600

    cleanup

--- springboot-kotlin/src/main/kotlin/tech/mrwconsulting/springboot/MyApplication.kt
+++ springboot-kotlin/src/main/kotlin/tech/mrwconsulting/springboot/MyApplication.kt
@@ -4,8 +4,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@RestController
 class MyApplication
 
 fun main(args: Array<String>) {
 	runApplication<MyApplication>(*args)
+
+	@GetMapping("/")
+	index() {
+		return "Welcome to Shinto pipeline samples";
+	}
 }
--- springboot-maven/src/main/java/tech/mrwconsulting/springboot/MyApplication.java
+++ springboot-maven/src/main/java/tech/mrwconsulting/springboot/MyApplication.java
@@ -9,13 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class MyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MyApplication.class, args);
 	}
 	@GetMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot!";
+		return "Welcome to Shinto pipeline samples";
 	}
-
 }
