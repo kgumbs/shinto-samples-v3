a0b9dcf4b3ab020f3b061a66da1998750d922679
Mark Washburn <mark@mrwconsulting.tech>
2024-02-02 13:23:51 -0600

    updated gradle

--- springboot-gradle/src/main/java/tech/mrwconsulting/springboot/MyApplication.java
+++ springboot-gradle/src/main/java/tech/mrwconsulting/springboot/MyApplication.java
@@ -2,12 +2,20 @@ package tech.mrwconsulting.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class MyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyApplication.class, args);
+	}
+	@GetMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
 	}
 
 }
